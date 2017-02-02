5d9d9c986dd658bffd81b273408c992f4e4e3441
Carlos Agüero <caguero@osrfoundation.org>
2017-02-02 19:01:19 +0100

    Add tinyxml2 dependency, CMake boilerplate and a .xodr test file

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -1,6 +1,6 @@
 include (${project_cmake_dir}/Utils.cmake)
 include (CheckCXXSourceCompiles)
-
+include (FindPkgConfig)
 include (${project_cmake_dir}/FindOS.cmake)
 
 ########################################
@@ -20,6 +20,38 @@ if (NOT WIN32)
     message(STATUS "Looking for ignition-math2-config.cmake - found")
     include_directories(${IGNITION-MATH_INCLUDE_DIRS})
     link_directories(${IGNITION-MATH_LIBRARY_DIRS})
+  endif()
+endif()
+
+########################################
+# Find tinyxml2 in unix platforms
+# In Windows we expect a call from configure.bat script with the paths
+if (NOT WIN32)
+  pkg_check_modules(tinyxml2 tinyxml2)
+  if (NOT tinyxml2_FOUND)
+      find_path (tinyxml2_INCLUDE_DIRS tinyxml2.h ${tinyxml2_INCLUDE_DIRS} ENV CPATH)
+      find_library(tinyxml2_LIBRARIES NAMES tinyxml2)
+      set (tinyxml2_FAIL False)
+      if (NOT tinyxml2_INCLUDE_DIRS)
+        message (STATUS "Looking for tinyxml2 headers - not found")
+        set (tinyxml2_FAIL True)
+      endif()
+      if (NOT tinyxml2_LIBRARIES)
+        message (STATUS "Looking for tinyxml2 library - not found")
+        set (tinyxml2_FAIL True)
+      endif()
+      if (NOT tinyxml2_LIBRARY_DIRS)
+        message (STATUS "Looking for tinyxml2 library dirs - not found")
+        set (tinyxml2_FAIL True)
+      endif()
+  endif()
+
+  if (tinyxml2_FAIL)
+    message (STATUS "Looking for tinyxml2.h - not found")
+    BUILD_ERROR("Missing: libtinyxml2-dev")
+  else()
+    include_directories(${tinyxml2_INCLUDE_DIRS})
+    link_directories(${tinyxml2_LIBRARY_DIRS})
   endif()
 endif()
 
